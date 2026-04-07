f65515c0d86c35d5ded29268d314e72a69faef40
Inkt13 <138111203+Inkt13@users.noreply.github.com>
2026-04-07 17:03:28 +0800

    chore: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  Serial.write("AAAAAAAAAAAAAAAAAAA \n");
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
