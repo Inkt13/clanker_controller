ffaf61dcd194b934a93e55d025dde445fdbf9e62
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-04-23 22:04:49 +0800

    fix: servo pin not set to output

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,24 @@ void setup()
   Serial.begin(115200);
 
   robot.initDisplay();
-  robot.displayStartupScreen();
+  // robot.displayStartupScreen();
 
   pinMode(ARM_MOTOR_IN1_PIN, OUTPUT);
   pinMode(ARM_MOTOR_IN2_PIN, OUTPUT);
   pinMode(ARM_MOTOR_IN3_PIN, OUTPUT);
   pinMode(ARM_MOTOR_IN4_PIN, OUTPUT);
 
+  pinMode(ARM_MOTOR_DOWN_SENSOR_PIN, INPUT);
+  pinMode(ARM_MOTOR_UP_SENSOR_PIN,   INPUT);
+  
+  pinMode(CLAW_SERVO_PIN, OUTPUT);
+
   robot.initClaw();
+  robot.setClawServoAngle(20);
+  // delay(2000);
+  // robot.displayTaskCode("123+123");
+
+  
 }
 
 // byte data sent from the Raspberry PI 5
@@ -28,6 +38,5 @@ void loop()
     SerialDecoder::handleSerialData(serialData);
   }
   robot.updateArmMotor();
-  robot.updateScroll();
-  robot.displayStartupScreen();
+  // robot.updateScroll();
 }
